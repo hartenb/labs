7daccc89fc2d8cc51f2342ef359693cd8c8c210e
Viktor <viktordoth@gmail.com>
2014-10-01 11:26:37 +0200

    StringCalculatorKata
    Kata

---
+++ FFCG.StringCalculator/FFCG.StringCalculator/StringCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFCG.StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "")
+                return 0;
+            if (numbers.Contains(","))
+            {
+                int sum;
+                int[] ints = numbers.Split(',').Select(int.Parse).ToArray();
+                sum = ints.Sum();
+                return sum;
+            }
+            return Convert.ToInt32(numbers);
+        }
+    }
+
+    [TestFixture]
+    public class StringCalculatorTests
+    { 
+
+        [Test]
+        public void Add_WithEmptyString_Return0()
+        {
+            ArrangeActAssert("", 0);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void Add_WithSingleNumber_ReturnThatNumber(string numbers, int expected)
+        {
+            ArrangeActAssert(numbers, expected);
+        }
+
+        [TestCase("1, 2", 3)]
+        public void Add_WithMultipleNumbers_returnSum(string numbers, int expected)
+        {
+            ArrangeActAssert(numbers, expected);
+        }
+
+        private static void ArrangeActAssert(string numbers, int expected)
+        {
+            var calculator = new StringCalculator();
+
+            var result = calculator.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
