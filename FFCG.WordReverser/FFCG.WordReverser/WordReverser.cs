9e866d5488109c2c969b4f1c554ddebe949b2c8e
Viktor <viktordoth@gmail.com>
2014-10-02 10:19:29 +0200

    WordReverser
    Reversing two words

--- FFCG.WordReverser/FFCG.WordReverser/WordReverser.cs
+++ FFCG.WordReverser/FFCG.WordReverser/WordReverser.cs
@@ -11,6 +11,12 @@ namespace FFCG.WordReverser
     {
         public string ReverseWords(string text)
         {
+            if (text.Contains(" "))
+            {
+                string[] inputString = text.Split(Convert.ToChar(' '));
+                text = inputString[1] + ' ' + inputString[0];
+                return text;
+            }
             return text;
         }
     }
@@ -18,15 +24,25 @@ namespace FFCG.WordReverser
     public class WordReverserTests
     {
         //1.	En mening med endast ett ord ska returnera ordet i sin urpsrungsform, tex: ReverseWords(”hello”) -> ”hello”
-
-        [TestCase("Hello", "Hello")]
+        [TestCase("hello", "hello")]
         public void ReturnWordAsItComes(string text, string expected)
         {
             var reverser = new WordReverser();
 
             var result = reverser.ReverseWords(text);
 
-            Assert.AreEqual(text, expected);
+            Assert.AreEqual(result, expected);
+        }
+
+        //En mening med två ord skall returnera en mening med orden i omvänd ordning: ReverseWords(”hello world”) -> ”world hello”
+        [TestCase("hello world", "world hello")]
+        public void ReturnTwoWordsInReverseOrder(string text, string expected)
+        {
+            var reverser = new WordReverser();
+
+            var result = reverser.ReverseWords(text);
+
+            Assert.AreEqual(result, expected);
         }
     }
 }
